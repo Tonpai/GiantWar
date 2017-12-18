621e1f6d671b9784796685fa5f53726507abd8ca
Tonpai <itonpai.bkk@gmail.com>
2017-12-18 08:49:36 +0700

    Update state and normal js files.

---
+++ game.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import {test} from "/states/loadSrc.js";
+
+window.onload = function(){
+    var game = new Phaser.Game(600,400); 
+
+}
---
+++ states/loadSrc.js
@@ -0,0 +1,7 @@
+var state = {
+    create : function(){
+        console.log("That's my awesome game");
+    }
+}
+
+var test = 3;
